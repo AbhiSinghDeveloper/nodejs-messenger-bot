3ba7ef247ecf9519ab7693622c990439c4e5d671
AbhiSinghDeveloper <abhisinghdeveloper@outlook.com>
2022-02-08 23:10:47 +0530

    added routes

--- routes/web.js
+++ routes/web.js
@@ -1,12 +1,19 @@
 const express = require("express");
-
+const chatBotController = require("../controllers/chatBotController");
+const messagesController = require("../controllers/messagesController");
 
 let router = express.Router();
 
 let initWebRoutes = async (app) => {
-    
+    router.get("/messages", messagesController.getMessages);
+    router.get("/messages/:messId", messagesController.getMessageId);
+
     router.get("/webhook", chatBotController.getWebhook);
+
+    router.post("/", chatBotController.postMessage);
     router.post("/webhook", chatBotController.postWebhook);
 
     return app.use("/", router);
 };
+
+module.exports = initWebRoutes;
