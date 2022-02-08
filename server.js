299857aa3cbc1d2f48be5f6f3a6184cfcf85ac35
AbhiSinghDeveloper <abhisinghdeveloper@outlook.com>
2022-02-08 23:12:55 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const initWebRoute = require("./routes/web");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 let app = express();
 
 app.use(bodyParser.json());
+app.use(cors({
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+ }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 initWebRoute(app);
 
